7b0f67edfa4acfd8038dddeb379fad7e082b9130
rikelmylucena2 <rikelmylucena2@gmail.com>
2025-11-28 22:59:23 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id': 1,
+            'titulo': 'Bolo de Chocolate',
+            'descricao': 'Um classico irresistivel.',
+            'foto': ''
+        },
+        {
+            'id': 2,
+            'titulo': 'Bolo de Morango',
+            'descricao': 'Bolo de morango delicioso',
+            'foto': ''
+        }
+    ]
+    return cardapio
